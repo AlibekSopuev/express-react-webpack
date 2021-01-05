7d9239f6621ff160c13175a774cad91271d30fb3
asopuev <7/>
2021-01-05 14:50:27 +0300

    created repository

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from "axios";
+
+export const App = () => {
+    React.useEffect(() => {
+        let fetchData = () => {
+            return new Promise(() => {
+                axios.get('api/todos');
+            })
+        };
+        fetchData().then((result) => {
+            console.log(result)
+        });
+    }, [])
+    return <div>WELCOME TO REACT APP!!</div>;
+};
